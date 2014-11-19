29b0ea033de39e6dfe75ed14dec1246cb31a9e1c
Rene Bannat <r.bannat@gmail.com>
2014-11-19 17:45:38 +0100

    add trip as resource route

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,9 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', 'HomeController@showWelcome');
+
+//Trip Route
+Route::resource('trip', 'TripController');
+
+
