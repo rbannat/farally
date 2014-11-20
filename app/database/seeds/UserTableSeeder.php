b6df2f70e48c22070de714647bb5ce4a571ab210
Stefan Schwartze <stefan.schwartze@gmail.com>
2014-11-20 14:09:22 +0100

    created some example data for UserTable Seeder

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -2,19 +2,32 @@
 
 class UserTableSeeder extends Seeder {
 
-    public function run()
-    {
-        DB::table('users')->delete();
+	public function run()
+	{
+		DB::table('users')->delete();
 
-        User::create(array(
-            'username' => 'firstuser',
-            'password' => Hash::make('first_password')
-        ));
+		User::create(array(
+			'username' => 'stefanschwartze',
+			'password' => Hash::make('stefan'),
+			'forename' => 'Stefan',
+			'lastname' => 'Schwartze',
+			'email' => 'hello@stefanschwartze.com',
+			'birthdate' => '1992-06-10',
+			'gender' => 'male',
+			'about' => 'Hi. I am Stefan. I like to troll around in the web',
+			'profile_image' => '',
+		));
 
-        User::create(array(
-            'username' => 'seconduser',
-            'password' => Hash::make('second_password')
-        ));
-    }
+		User::create(array(
+            'username' => 'rennitlb',
+            'password' => Hash::make('rene'),
+            'forename' => 'Rene',
+            'lastname' => 'Bannat',
+            'email' => 'rennitlb@gmail.com',
+            'birthdate' => '1990-01-01',
+            'gender' => 'male',
+            'about' => 'Ich mag Toastbrot.',
+            'profile_image' => '',
+        ));	}
 
 }
