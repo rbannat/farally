41013b67b14846185c1f46fd613058a768dcf398
Stefan Schwartze <stefan.schwartze@gmail.com>
2014-12-01 17:33:00 +0100

    updated main template

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href='http://fonts.googleapis.com/css?family=ABeeZee|Oxygen|Open+Sans' rel='stylesheet' type='text/css'>
 		<title>Farally</title>
 		{{ HTML::style('0.1/css/main.css')}}
 	</head>
@@ -10,16 +11,17 @@
 	<body>
 
 		<div class="headarea">
-			<h1 class="headarea_title">Farally App</h1>
-			<img class="headarea_logo" src="0.1/img/logo.png">
+			<div class="head_colorfilm"></div>
+			<img class="headarea_logo" src="0.1/img/logo.jpg">
+			@if(Session::has('message'))
+				<h3 class="headarea_title">{{ Session::get('message') }}</h3>
+			@endif
 		</div>
 
-		{{ $content }}
+		<div class="content">
 
-		<div class="container">
-			@if(Session::has('message'))
-					<p class="">{{ Session::get('message') }}</p>
-			@endif
+			{{ $content }}
+
 		</div>
 
 	</body>
