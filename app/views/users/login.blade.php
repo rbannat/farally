b0908a371c719c7770b8f380aa0a12c9d49859aa
Stefan Schwartze <stefan.schwartze@gmail.com>
2014-12-01 17:31:17 +0100

    updated login form

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,9 +1,13 @@
-{{ HTML::link('register', 'Register') }}
-{{ Form::open(array('url'=>'/login', 'class'=>'')) }}
-    <h2 class="">Please Login</h2>
+{{ Form::open(array('url'=>'/login', 'class'=>'padding')) }}
+    <h3 class="text float_row center">Please Sign In</h3>
 
-    {{ Form::text('email', null, array('class'=>'', 'placeholder'=>'Email Address')) }}
-    {{ Form::password('password', array('class'=>'', 'placeholder'=>'Password')) }}
+    {{ Form::text('email', null, array('class'=>'float_row input input_text', 'placeholder'=>'Email Address')) }}
+    {{ Form::password('password', array('class'=>'float_row input input_text', 'placeholder'=>'Password')) }}
 
-    {{ Form::submit('Login', array('class'=>''))}}
+    {{ Form::submit('Login', array('class'=>'float_row button button_green'))}}
 {{ Form::close() }}
+
+<div class="padding">
+	<p class="text float_row center">OR</p>
+	{{ HTML::link('register', 'Sign Up') }}
+</div>
