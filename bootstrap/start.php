db13b3b07a0934278d5d4033b2a37ebbffabd271
Stefan Schwartze <stefan.schwartze@gmail.com>
2014-12-01 17:31:49 +0100

    added local machines

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Stefans-MacBook.local', 'Rene-Lenovo', 'RenniPC'),
+	'local' => array('Stefans-MacBook.local', 'stefans-macbook', 'Rene-Lenovo', 'RenniPC'),
 
 ));
 /*
