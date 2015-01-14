88f5e9bfa1adf5ce9551e2947387a4371b81539f
Stefan Schwartze <stefan.schwartze@gmail.com>
2015-01-14 01:04:37 +0100

    updated header

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -1,29 +1,26 @@
-<header class="section header">
+<header class="section header padding">
 
 	<div class="section_container">
 
-		<i class="fa fa-paper-plane fa-stack-1x fa-inverse"></i>
+		<i class="header_logo fa fa-paper-plane fa-2x"></i>
 
-	@if(Session::has('message'))
-		<h3 class="headarea_title">{{ Session::get('message') }}</h3>
-	@endif
+		<h3 class="header_title">Dashboard</h3>
 
-	@if(Auth::check())
-
-		<a class="menu-link">
-			<i class="fa fa-navicon fa-4x"></i>
+		<a class="header_menu-link">
+			<i class="fa fa-navicon fa-2x"></i>
 		</a>
 
-		<nav id="site-menu" class="dropdown-menu">
-			<ul>
-				<li> {{ HTML::link('/', 'Dashboard', array('class'=>'button input_text')) }} </li>
-				<li> {{ HTML::link('trips/create', 'Create Trip', array('class'=>'button input_text')) }} </li>
-				<li> {{ HTML::link('users/trips', 'Your Trips', array('class'=>'button input_text')) }} </li>
-				<li> {{ HTML::link('users/$user->id', 'Profile', array('class'=>'button input_text')) }} </li>
-				<li> {{ HTML::link('users/$user->id', 'Settings', array('class'=>'button input_text')) }} </li>
-				<li> {{ HTML::link('/logout', 'Logout', array('class'=>'button input_text')) }} </li>
-			</ul>
-		</nav>
-	@endif
 	</div>
+
 </header>
+
+<nav id="site-menu" class="dropdown-menu padding">
+	<ul class="list">
+		<li> {{ HTML::link('/', 'Dashboard', array('class'=>'button input_text')) }} </li>
+		<li> {{ HTML::link('trips/create', 'Create Trip', array('class'=>'button input_text')) }} </li>
+		<li> {{ HTML::link('users/trips', 'Your Trips', array('class'=>'button input_text')) }} </li>
+		<li> {{ HTML::link('users/$user->id', 'Profile', array('class'=>'button input_text')) }} </li>
+		<li> {{ HTML::link('users/$user->id', 'Settings', array('class'=>'button input_text')) }} </li>
+		<li> {{ HTML::link('/logout', 'Logout', array('class'=>'button input_text')) }} </li>
+	</ul>
+</nav>
