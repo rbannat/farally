38f175990f3441699a2737a41f263bd5a4bdd9bb
Stefan Schwartze <stefan.schwartze@gmail.com>
2015-01-19 16:42:12 +0100

    updated text style

--- app/models/Trip.php
+++ app/models/Trip.php
@@ -2,14 +2,14 @@
 
 class Trip extends Eloquent {
 	protected $fillable = [
-	'title', 
-	'description', 
-	'user_id',
-	'start_date',
-	'end_date', 
-	'destination', 
-	'start', 
-	'max_travellers'
+		'title',
+		'description',
+		'user_id',
+		'start_date',
+		'end_date',
+		'destination',
+		'start',
+		'max_travellers'
 	];
 
 	public function user(){
