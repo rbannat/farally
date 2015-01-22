8196f5462350cd3205479268a767a280203323d7
Stefan Schwartze <stefan.schwartze@gmail.com>
2015-01-22 01:30:24 +0100

    removed link to user profile because of buggy behavior

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -18,8 +18,8 @@
 	<ul class="list">
 		<li> {{ HTML::link('/', 'Dashboard', array('class'=>'button input_text')) }} </li>
 		<li> {{ HTML::link('trips/create', 'Create Trip', array('class'=>'button input_text')) }} </li>
-		<li> {{ HTML::link('users/' . $user->id . '/trips', 'Your Trips', array('class'=>'button input_text')) }} </li>
-		<li> {{ HTML::link('users/' . $user->id, 'Profile', array('class'=>'button input_text')) }} </li>
+		<li> {{ HTML::link('users/trips', 'Your Trips', array('class'=>'button input_text')) }} </li>
+		<li> {{ HTML::link('users/', 'Profile', array('class'=>'button input_text')) }} </li>
 		<li> {{ HTML::link('users', 'Settings', array('class'=>'button input_text')) }} </li>
 		<li> {{ HTML::link('/logout', 'Logout', array('class'=>'button input_text')) }} </li>
 	</ul>
