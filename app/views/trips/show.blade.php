799f82a360d46c2b22b8765fb6b763ea69917308
Stefan Schwartze <stefan.schwartze@gmail.com>
2015-01-22 01:31:27 +0100

    switched google maps api from embed to javascript api and updated styles

--- app/views/trips/show.blade.php
+++ app/views/trips/show.blade.php
@@ -5,21 +5,26 @@
 	<section class="section trip">
 		<header class="section_header">
 			<div class="overlay"></div>
-			<div class="map_embed" style="width: 100%; overflow: hidden; height: 100%;">
-				<iframe class="embed_map" width="100%" height="1000" frameborder="0" style="border:0; margin-top: -65%;" src="https://www.google.com/maps/embed/v1/place?q={{{ $trip->destination }}}&key=AIzaSyDpAQDFH6Tl19LOSnXuZeSbEyhZrYEmdrw&zoom=12"></iframe>
+			<div class="map_embed" style="width: 100%; overflow: hidden;">
+				<div id="map-canvas" class="embed_map"></div>
 			</div>
 			<div class="section_container">
+				<div class="header-toggle">
+					<i class="fa fa-expand fa-2x"></i>
+					<i class="fa fa-compress fa-2x"></i>
+				</div>
 				<div class="section_container_head">
 					<h1 class="section_container_title">{{{ $trip->title }}}<br><small><i class="fa fa-user fa-1x"></i>{{{ $trip->user->username}}}</small></h1>
-					<p>{{{ $trip->destination }}}</p>
+					<p id="destination">{{{ $trip->destination }}}</p>
 				</div>
 			</div>
 		</header>
+	</section>
+	<section class="section content">
 		<div class="section_metabox">
 			<ul class="section_metabox_list list">
 				<li class="section_metabox_list_item destination"><i class="fa fa-location-arrow"></i>{{{ $trip->destination }}}</li>
-				<li class="section_metabox_list_item start_date"><i class="fa fa-calendar"></i>{{{ $trip->start_date }}}</li>
-				<li class="section_metabox_list_item end_date"><i class="fa fa-calendar"></i>{{{ $trip->end_date }}}</li>
+				<li class="section_metabox_list_item start_date"><i class="fa fa-calendar"></i>{{{ $trip->start_date }}} - {{{ $trip->end_date }}}</li>
 				<li class="section_metabox_list_item transport"><i class="fa fa-car"></i>{{{ $trip->transport }}}</li>
 			</ul>
 		</div>
@@ -27,19 +32,54 @@
 			<article>
 				<h3 class="section_container_subtitle">Description</h3>
 				<p>{{{ $trip->description }}}</p>
-				<h3 class="section_container_subtitle">Trave time</h3>
-					<p>{{{ $trip->start_date }}}</p>
-					<p>{{{ $trip->end_date }}}</p>
 				<h3 class="section_container_subtitle">Travellers</h3>
 					<p> / {{{ $trip->max_travellers }}}</p>
-					<p>{{{ $trip->transport }}}</p>
 			</article>
 		</div>
 	</section>
 		{{ HTML::script('0.1/js/jquery/jquery-2.1.3.min.js') }}
 		{{ HTML::script('0.1/js/main.js')}}
-		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
+		<script type="text/javascript">
+			$('.header-toggle').click(function() {
+		    	$('.section_header').toggleClass('expand');
+		    });
+		</script>
+		<script>
 
+			function initialize() {
+				var geocoder, map;
+				var address = document.getElementById('destination').innerHTML;
+			    geocoder = new google.maps.Geocoder();
+			    geocoder.geocode({
+			        'address': address
+			    }, function(results, status) {
+			        if (status == google.maps.GeocoderStatus.OK) {
+			            var myOptions = {
+			                zoom: 10,
+			                center: results[0].geometry.location,
+			                mapTypeId: google.maps.MapTypeId.ROADMAP,
+			    			disableDefaultUI: true
+			            }
+			            map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
+
+			            var marker = new google.maps.Marker({
+			                map: map,
+			                position: results[0].geometry.location
+			            });
+			        }
+			    });
+			}
+
+			function loadScript() {
+				var script = document.createElement('script');
+				script.type = 'text/javascript';
+				script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' + 'callback=initialize';
+				document.body.appendChild(script);
+			}
+
+			window.onload = loadScript;
+
+		</script>
 </body>
 @stop
 
