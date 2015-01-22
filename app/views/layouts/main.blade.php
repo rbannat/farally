0e14e56f0b9e968dcaf762dbc1044973d5a916fc
Stefan Schwartze <stefan.schwartze@gmail.com>
2015-01-22 01:29:23 +0100

    repaired webfont-link;

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -5,8 +5,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="apple-mobile-web-app-capable" content="yes">
 		<title>Farally</title>
-		<link href='http://fonts.googleapis.com/css?family=Lato:400,700|Roboto:400,300,700|Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Lato:400,700|Roboto:400,300,700|Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
 		{{ HTML::style('0.1/css/normalize.css')}}
 		{{ HTML::style('0.1/css/font-awesome.min.css')}}
 		{{ HTML::style('0.1/css/main.css')}}
