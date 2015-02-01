05fafa06faed4c4d954f84a472122d50b2adbe1a
Rene Bannat <r.bannat@gmail.com>
2015-02-01 18:43:15 +0100

    fix user edit profile route

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -134,19 +134,22 @@ class UsersController extends BaseController {
 	 */
 	public function edit( $user_id = null )
 	{
-		if (Auth::id() === $user_id) {
+		if (Auth::check()) {
+			if (Auth::id() == $user_id){
 			// get the user
-			$user = User::find($user_id);
+				$user = User::find($user_id);
 
 			// show the edit form and pass the user
-			return View::make('users.edit')
-			->with('user', $user);
-		} else {
-			$user = User::findOrFail($user_id);
-			$trips = Trip::where('user_id', '=', $user_id)->get();
-			return View::make('users.profile')->with('user', $user)->with('trips', $trips);
+				return View::make('users.edit')
+				->with('user', $user);
+			}  else {
+				$user = User::findOrFail($user_id);
+				$trips = Trip::where('user_id', '=', $user_id)->get();
+				return View::make('users.profile')->with('user', $user)->with('trips', $trips);
+			}
 		}
 	}
+
 
 	/**
 	    * Update the specified resource in storage.
