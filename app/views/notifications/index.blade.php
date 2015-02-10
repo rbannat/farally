e59565136624839f89dc46b39b7665c674c07aa0
Stefan Schwartze <stefan.schwartze@gmail.com>
2015-02-10 23:11:20 +0100

    corrected structure of notification files

--- app/views/notifications/index.blade.php
+++ app/views/notifications/index.blade.php
@@ -1,11 +1,8 @@
 @extends('layouts.main')
 @section('content')
 <body class="dashboard">
-	@include('partials.header')
-	@include('partials.tripsearch')
-	<section class="section ">
-		@include('partials.notificationlist')
-	</section>
+	@include('partials.header', array('title'=>'Notifications'))
+	@include('partials.notificationlist')
 	{{ HTML::script('0.1/js/jquery/jquery-2.1.3.min.js') }}
 	{{ HTML::script('0.1/js/main.js')}}
 	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
--- app/views/partials/notificationlist.blade.php
+++ app/views/partials/notificationlist.blade.php
@@ -14,3 +14,25 @@ Request:
 </p>
 @endif
 @endforeach
+
+
+
+<section class="section list">
+		<div class="section_container">
+			<h3 class="section_container_title_small "><i class="fa fa-clock-o fa-1x"></i>Notifications</h3>
+			@if(count($notifications))
+				@foreach($notifications as $notification)
+				<div class="notification {{ $notification->type }}">
+				    <div class="notification_title">{{ $notification->subject }}</div>
+				    <div class="body">{{ $notification->body }}</div>
+				    @if($notification->type = 'request')
+						<div class="request_buttons">
+							{{ HTML::link('', 'Accept', array('class'=>'button input_text')) }}
+							{{ HTML::link('', 'Decline', array('class'=>'button input_text')) }}
+						</div>
+				    @endif
+				</div>
+				@endforeach
+			@endif
+		</div>
+	</section>
