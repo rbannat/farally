4c3b9ec6062643cd5c4a6fc826aa6cb1963a7da6
Stefan Schwartze <stefan.schwartze@gmail.com>
2015-02-10 22:07:43 +0100

    added templates for notifications

--- app/views/partials/notifications.blade.php
+++ app/views/partials/notifications.blade.php
@@ -1,3 +1,19 @@
-<a href="">
-	{{{ count($notifications) }}} new notifications
-</a>
+<section class="section list">
+		<div class="section_container">
+			<h3 class="section_container_title_small "><i class="fa fa-clock-o fa-1x"></i>Notifications</h3>
+			@if(count($unreadNotifications))
+				@foreach($unreadNotifications as $notification)
+				<div class="notification {{ $notification->type }}">
+				    <div class="notification_title">{{ $notification->subject }}</div>
+				    <div class="body">{{ $notification->body }}</div>
+				    @if($notification->type = 'request')
+						<div class="request_buttons">
+							{{ HTML::link('', 'Accept', array('class'=>'button input_text')) }}
+							{{ HTML::link('', 'Decline', array('class'=>'button input_text')) }}
+						</div>
+				    @endif
+				</div>
+				@endforeach
+			@endif
+		</div>
+	</section>
