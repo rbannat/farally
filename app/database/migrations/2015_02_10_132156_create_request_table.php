e81b850fdfc409fb1800661e0bf4a5be66890be0
Rene Bannat <r.bannat@gmail.com>
2015-02-10 14:39:26 +0100

    add request table

--- app/database/migrations/2015_02_10_132156_create_request_table.php
+++ app/database/migrations/2015_02_10_132156_create_request_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTripUserMappingTable extends Migration {
+class CreateRequestTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,16 +12,16 @@ class CreateTripUserMappingTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('trip_user', function(Blueprint $table)
+		Schema::create('requests', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->timestamps();
 			$table->integer('trip_id')->unsigned();
 			$table->integer('user_id')->unsigned();
+			$table->tinyInteger('status')->default(0);
 			$table->foreign('user_id')->references('id')->on('users');
 			$table->foreign('trip_id')->references('id')->on('trips');
-		});
-	}
+		});	}
 
 	/**
 	 * Reverse the migrations.
@@ -30,7 +30,6 @@ class CreateTripUserMappingTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('trip_user');
-	}
+		Schema::drop('requests');	}
 
 }
