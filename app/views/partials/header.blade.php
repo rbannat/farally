1ddadd546db2f49f723e01c242f4e32b5d6970fb
Rene Bannat <r.bannat@gmail.com>
2015-02-11 23:07:13 +0100

    delete settingsmenu item

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -20,7 +20,6 @@
 		<li> {{ HTML::link('trips/create', 'Create Trip', array('class'=>'button input_text')) }} </li>
 		<li> {{ HTML::linkRoute('users.user_trips', 'Your Trips', array(Auth::id()), array('class' => 'button input_text')) }} </li>
 		<li> {{ HTML::linkRoute('users.one', 'Profile', array(Auth::id()), array('class' => 'button input_text')) }} </li>
-		<li> {{ HTML::link('users', 'Settings', array('class'=>'button input_text')) }} </li>
 		<li> {{ HTML::link('logout', 'Logout', array('class'=>'button input_text')) }} </li>
 	</ul>
 </nav>
--- app/views/partials/menu.blade.php
+++
@@ -1,6 +0,0 @@
-<ul class="menu">
-	<li><a href="#">Link</a></li>
-	<li><a href="#">Link</a></li>
-	<li><a href="#">Link</a></li>
-	<li><a href="#">Link</a></li>
-</ul>
