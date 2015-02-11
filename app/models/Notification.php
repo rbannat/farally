2df771f075d4899f8004fb49790f16a666324fb4
Rene Bannat <r.bannat@gmail.com>
2015-02-11 18:16:25 +0100

    adding display infos to notifications

--- app/models/Notification.php
+++ app/models/Notification.php
@@ -14,6 +14,18 @@ class Notification extends Eloquent {
 		return $this->belongsTo('User');
 	}
 
+	public function fromUser()
+	{
+		return $this->belongsTo('User', 'from_user');
+	}
+
+	public function regardingTrip()
+	{
+		return $this->belongsTo('Trip', 'trip_id');
+	}
+
+
+	//Setter Methods
 	public function withSubject($subject)
 	{
 		$this->subject = $subject;
--- app/views/partials/notificationlist.blade.php
+++ app/views/partials/notificationlist.blade.php
@@ -1,38 +1,34 @@
-@foreach($notifications as $notification)
-@if($notification->type = 'request')
-<p>
-Request:
-	<a href="">
-		{{{$notification->subject}}}
-	</a>
-	<a href="">
-		{{{$notification->from_user}}}
-	</a>
-	<a href="">
-		{{{$notification->trip_id}}}
-	</a>
-</p>
-@endif
-@endforeach
-
-
-
 <section class="section list">
-		<div class="section_container">
-			<h3 class="section_container_title_small "><i class="fa fa-clock-o fa-1x"></i>Notifications</h3>
-			@if(count($notifications))
-				@foreach($notifications as $notification)
-				<div class="notification {{ $notification->type }}">
-				    <div class="notification_title">{{ $notification->subject }}</div>
-				    <div class="body">{{ $notification->body }}</div>
-				    @if($notification->type = 'request')
-						<div class="request_buttons">
-							{{ HTML::link('', 'Accept', array('class'=>'button input_text')) }}
-							{{ HTML::link('', 'Decline', array('class'=>'button input_text')) }}
-						</div>
-				    @endif
-				</div>
-				@endforeach
+	<div class="section_container">
+		<h3 class="section_container_title_small "><i class="fa fa-clock-o fa-1x"></i>Notifications</h3>
+		@if(count($notifications))
+		@foreach($notifications as $notification)
+		<div class="notification {{ $notification->type }}">
+			@if($notification->type == 'request')
+			<p>{{{ $notification->fromUser->forename }}} wants to join your Trip "{{{$notification->regardingTrip->title}}}":</p>
+			<div class="notification_title">{{ $notification->subject }}</div>
+			<div class="body">{{ $notification->body }}</div>
+			<div class="request_buttons">
+				{{ HTML::link('', 'Accept', array('class'=>'button input_text')) }}
+				{{ HTML::link('', 'Decline', array('class'=>'button input_text')) }}
+			</div>
+			
+
+			@elseif($notification->type == 'accepted')
+
+			<p>{{{ $notification->fromUser->forename }}} accepted your Triprequest for "{{{$notification->regardingTrip->title}}}":</p>
+			<div class="notification_title">{{ $notification->subject }}</div>
+			<div class="body">{{ $notification->body }}</div>
+		
+			@elseif($notification->type == 'declined')
+
+			<p>{{{ $notification->fromUser->forename }}} declined your Triprequest for "{{{$notification->regardingTrip->title}}}":</p>
+			<div class="notification_title">{{ $notification->subject }}</div>
+			<div class="body">{{ $notification->body }}</div>
+		
 			@endif
 		</div>
-	</section>
+		@endforeach
+		@endif
+	</div>
+</section>
